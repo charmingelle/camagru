b4867582b2e4b57cf6c3170ab7b1812049a2e90d
charmingelle <revenkoaa89@gmail.com>
2018-05-05 19:10:04 +0300

    added signin buttons to home page

--- views/home.php
+++ views/home.php
@@ -6,7 +6,13 @@
 		<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>
 	</head>
 	<body>
-		<header>Camagru Gallery</header>
+		<header>
+			<div>Camagru Gallery</div>
+			<div>
+				<button id='signin-button'>Sign in</button>
+				<button id='reset-password-button'>Reset password?</button>
+			</div>
+		</header>
 		<div id='main'>
 			<div id='gallery'></div>
 		</div>
