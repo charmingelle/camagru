7df172c1978815954fcbcb67f66d11b818b473cb
charmingelle <revenkoaa89@gmail.com>
2018-06-22 15:07:55 +0300

    added condition to Route class

--- app/core/Route.class.php
+++ app/core/Route.class.php
@@ -9,31 +9,70 @@ class Route {
 		return explode('@', $controller_name_and_method)[1];
 	}
 
-	public static function get($uri, $controller_name_and_method) {
-		$url = parse_url($_SERVER['REQUEST_URI'])['path'];
-		$controller_name = self::_getControllerName($controller_name_and_method);
-		$controller_method = self::_getControllerMethod($controller_name_and_method);
-
-		if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
-			return ;
+	private static function _isTrueCondition($condition) {
+		if ($condition === 'signed in'
+			&& isset($_SESSION['auth-data']['login'])
+			&& $_SESSION['auth-data']['login'] !== '') {
+				return true;
 		}
-		if ($url === $uri) {
-			return $controller_name::$controller_method();
+		return false;
+	}
+
+	public static function get(array $params) {
+		if (!isset($params['condition']) || self::_isTrueCondition($params['condition'])) {
+			$url = parse_url($_SERVER['REQUEST_URI'])['path'];
+			$controller_name = self::_getControllerName($params['controller']);
+			$controller_method = self::_getControllerMethod($params['controller']);
+	
+			if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+				return ;
+			}
+			if ($url === $params['uri']) {
+				return $controller_name::$controller_method();
+			}
 		}
 	}
 	
-	public static function post($uri, $controller_name_and_method) {
-		$url = parse_url($_SERVER['REQUEST_URI'])['path'];
-		$controller_name = self::_getControllerName($controller_name_and_method);
-		$controller_method = self::_getControllerMethod($controller_name_and_method);
+	public static function post(array $params) {
+		if (!isset($params['condition']) || self::_isTrueCondition($params['condition'])) {
+			$url = parse_url($_SERVER['REQUEST_URI'])['path'];
+			$controller_name = self::_getControllerName($params['controller']);
+			$controller_method = self::_getControllerMethod($params['controller']);
 
-		if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-			return ;
-		}
-		if ($url === $uri) {
-			return $controller_name::$controller_method();
+			if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+				return ;
+			}
+			if ($url === $params['uri']) {
+				return $controller_name::$controller_method();
+			}
 		}
 	}
+
+	// public static function get($uri, $controller_name_and_method) {
+	// 	$url = parse_url($_SERVER['REQUEST_URI'])['path'];
+	// 	$controller_name = self::_getControllerName($controller_name_and_method);
+	// 	$controller_method = self::_getControllerMethod($controller_name_and_method);
+
+	// 	if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
+	// 		return ;
+	// 	}
+	// 	if ($url === $uri) {
+	// 		return $controller_name::$controller_method();
+	// 	}
+	// }
+	
+	// public static function post($uri, $controller_name_and_method) {
+	// 	$url = parse_url($_SERVER['REQUEST_URI'])['path'];
+	// 	$controller_name = self::_getControllerName($controller_name_and_method);
+	// 	$controller_method = self::_getControllerMethod($controller_name_and_method);
+
+	// 	if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+	// 		return ;
+	// 	}
+	// 	if ($url === $uri) {
+	// 		return $controller_name::$controller_method();
+	// 	}
+	// }
 
 	public static function redirect($uri) {
 		header('Location: ' . $uri);
--- app/routes/web.php
+++ app/routes/web.php
@@ -6,36 +6,68 @@ require_once(getRoot() . 'app/controllers/SiteMapController.php');
 require_once(getRoot() . 'app/core/Route.class.php');
 require_once(getRoot() . 'app/core/Message.php');
 
-Route::get('/', 'SiteMapController@showHome');
+Route::get(['uri' => '/', 'controller' => 'SiteMapController@showHome']);
 
-Route::get('/signup', 'AuthController@signup');
-Route::post('/signup', 'AuthController@sendVerification');
+Route::get(['uri' => '/signup', 'controller' => 'AuthController@signup']);
+Route::post(['uri' => '/signup', 'controller' => 'AuthController@sendVerification']);
 
-Route::get('/signin', 'SiteMapController@showSignin');
-Route::post('/signin', 'AuthController@signin');
+Route::get(['uri' => '/signin', 'controller' => 'SiteMapController@showSignin']);
+Route::post(['uri' => '/signin', 'controller' => 'AuthController@signin']);
 
-Route::get('/signout', 'AuthController@signout');
+Route::get(['uri' => '/signout', 'controller' => 'AuthController@signout']);
 
-Route::get('/changeEmail', 'SiteMapController@showChangeEmail');
-Route::post('/changeEmail', 'AuthController@changeEmail');
+Route::get(['uri' => '/changeEmail', 'controller' => 'SiteMapController@showChangeEmail']);
+Route::post(['uri' => '/changeEmail', 'controller' => 'AuthController@changeEmail']);
 
-Route::get('/changeLogin', 'SiteMapController@showChangeLogin');
-Route::post('/changeLogin', 'AuthController@changeLogin');
+Route::get(['uri' => '/changeLogin', 'controller' => 'SiteMapController@showChangeLogin']);
+Route::post(['uri' => '/changeLogin', 'controller' => 'AuthController@changeLogin']);
 
-Route::get('/changePassword', 'AuthController@showChangePassword');
-Route::post('/changePassword', 'AuthController@changePassword');
+Route::get(['uri' => '/changePassword', 'controller' => 'AuthController@showChangePassword']);
+Route::post(['uri' => '/changePassword', 'controller' => 'AuthController@changePassword']);
 
-Route::get('/forgotPassword', 'SiteMapController@showForgotPassword');
-Route::post('/forgotPassword', 'AuthController@sendResetLink');
+Route::get(['uri' => '/forgotPassword', 'controller' => 'SiteMapController@showForgotPassword']);
+Route::post(['uri' => '/forgotPassword', 'controller' => 'AuthController@sendResetLink']);
 
-Route::post('/photos', 'DataController@getPhotos');
+Route::post(['uri' => '/photos', 'controller' => 'DataController@getPhotos']);
 
-Route::get('/account', 'SiteMapController@showAccount');
+Route::get(['uri' => '/account', 'controller' => 'SiteMapController@showAccount']);
 
-Route::post('/stickers', 'DataController@getStickers');
+Route::post(['uri' => '/stickers', 'controller' => 'DataController@getStickers']);
 
-Route::post('/save-picture', 'DataController@savePicture');
+Route::post(['uri' => '/save-picture', 'controller' => 'DataController@savePicture']);
 
-Route::post('/userPictures', 'DataController@getUserPictures');
+Route::post(['uri' => '/userPictures', 'controller' => 'DataController@getUserPictures', 'condition' => 'signed in']);
+
+// Route::get('/', 'SiteMapController@showHome');
+
+// Route::get('/signup', 'AuthController@signup');
+// Route::post('/signup', 'AuthController@sendVerification');
+
+// Route::get('/signin', 'SiteMapController@showSignin');
+// Route::post('/signin', 'AuthController@signin');
+
+// Route::get('/signout', 'AuthController@signout');
+
+// Route::get('/changeEmail', 'SiteMapController@showChangeEmail');
+// Route::post('/changeEmail', 'AuthController@changeEmail');
+
+// Route::get('/changeLogin', 'SiteMapController@showChangeLogin');
+// Route::post('/changeLogin', 'AuthController@changeLogin');
+
+// Route::get('/changePassword', 'AuthController@showChangePassword');
+// Route::post('/changePassword', 'AuthController@changePassword');
+
+// Route::get('/forgotPassword', 'SiteMapController@showForgotPassword');
+// Route::post('/forgotPassword', 'AuthController@sendResetLink');
+
+// Route::post('/photos', 'DataController@getPhotos');
+
+// Route::get('/account', 'SiteMapController@showAccount');
+
+// Route::post('/stickers', 'DataController@getStickers');
+
+// Route::post('/save-picture', 'DataController@savePicture');
+
+// Route::post('/userPictures', 'DataController@getUserPictures');
 
 // Route::post('/userPictures', 'DataController@getUserPictures')->if(User,authentificated);
