ee5ac767d91e1192c5cbf861a9443dcb3929a3b6
charmingelle <revenkoaa89@gmail.com>
2018-07-02 18:25:45 +0300

    rewrote account.js in OOP style

--- public/js/account.js
+++ public/js/account.js
@@ -1,131 +1,164 @@
-const vh = v => {
-	let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+class Account {
+	constructor() {
+		this.video = document.getElementById('account-video');
+		this.preview = document.getElementById('preview');
+		this.stickersContainer = document.getElementById('account-stickers');
+		this.userPicturesContainer = document.getElementById('account-user-pictures');
+		this.scale = this.vmin(50);
+		this.canvas = document.createElement('canvas');
+		this.result = document.createElement('img');
+		
+		this.result.id = 'result';
 
-	return (v * h) / 100;
-}
-
-const vw = v => {
-	let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-
-	return (v * w) / 100;
-}
-
-const vmin = v => {
-	return Math.min(vh(v), vw(v));
-}
-
-const appendSticker = sources => {
-	if (sources) {
-		const images = sources.map(source => {
-			let image = document.createElement('img');
-			
-			image.src = source['url'];
-			image.classList.add('sticker');
-			return image;
-		});
-	
-		stickersContainer.append(...images);
+		this.removeAllChildren = this.removeAllChildren.bind(this);
+		this.vh = this.vh.bind(this);
+		this.vw = this.vw.bind(this);
+		this.vmin = this.vmin.bind(this);
+		this.appendSticker = this.appendSticker.bind(this);
+		this.deleteUserPicture = this.deleteUserPicture.bind(this);
+		this.appendUserPicture = this.appendUserPicture.bind(this);
+		this.reloadUserPicture = this.reloadUserPicture.bind(this);
+		this.renderCamera = this.renderCamera.bind(this);
+		this.takePicture = this.takePicture.bind(this);
+		this.renderStickers = this.renderStickers.bind(this);
+		this.addSticker = this.addSticker.bind(this);
+		this.savePicture = this.savePicture.bind(this);
+		this.render = this.render.bind(this);
 	}
-}
 
-// picture removing is here
+	removeAllChildren(elem) {
+		while (elem.firstChild) {
+			elem.removeChild(elem.firstChild);
+		}
+	}
 
-const appendUserPicture = sources => {
-	if (sources) {
-		const images = sources.map(source => {
-			let image = document.createElement('img');
-			
-			image.src = source['url'];
-			image.classList.add('user-picture');
-			image.addEventListener('click', () => {
-				fetch('/deleteUserPicture', {
-					method: 'POST',
-					credentials: 'include',
-					body: source['id']
-				})
-				.then(() => {
-					userPicturesContainer.removeChild(image);
-				})
+	vh(v) {
+		let h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+	
+		return (v * h) / 100;
+	}
+	
+	vw(v) {
+		let w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+	
+		return (v * w) / 100;
+	}
+	
+	vmin(v) {
+		return Math.min(this.vh(v), this.vw(v));
+	}
+	
+	appendSticker(sources) {
+		if (sources) {
+			const images = sources.map(source => {
+				let image = document.createElement('img');
+				
+				image.src = source['url'];
+				image.classList.add('sticker');
+				return image;
 			});
-			return image;
-		});
-	
-		userPicturesContainer.append(...images);
+		
+			this.stickersContainer.append(...images);
+		}
 	}
-}
 
-// picture removing is here
-
-const reloadUserPicture = () => {
-	while (userPicturesContainer.firstChild) {
-		userPicturesContainer.removeChild(userPicturesContainer.firstChild);
-	}
-	fetch('/userPictures', {
-		method: 'POST',
-		credentials: 'include'
-	})
-	.then(response => response.json())
-	.then(appendUserPicture)
-	.catch(error => console.log(error.message));
-}
-
-let video = document.getElementById('account-video');
-let preview = document.getElementById('preview');
-let stickersContainer = document.getElementById('account-stickers');
-let userPicturesContainer = document.getElementById('account-user-pictures');
-let scale = vmin(50);
-let canvas = document.createElement('canvas');
-let result = document.createElement('img');
-
-result.id = 'result';
-
-if (navigator.mediaDevices.getUserMedia) {
-	navigator.mediaDevices.getUserMedia({video: true})
-	.then(function(stream) {
-		video.srcObject = stream;
-	})
-	.catch(function(err0r) {
-		console.log('The camera cannot be used');
-	});
-}
-
-document.getElementById('account-capture-button').addEventListener('click', () => {
-	canvas.width = scale;
-	canvas.height = (video.videoHeight * scale) / video.videoWidth;
-	canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
-
-	result.src = canvas.toDataURL();
-	preview.appendChild(result);
-});
-
-fetch('/stickers', {
-	method: 'POST',
-	credentials: 'include'
-})
-.then(response => response.json())
-.then(appendSticker)
-.catch(error => console.log(error.message));
-
-reloadUserPicture();
-
-stickersContainer.addEventListener('click', event => {
-	while (preview.firstChild) {
-		preview.removeChild(preview.firstChild);
-	}
-	canvas.getContext('2d').drawImage(event.target, 0, 0, canvas.width, canvas.height);
-	result.src = canvas.toDataURL();
-	preview.appendChild(result);
-});
-
-document.getElementById('account-save-button').addEventListener('click', () => {
-	let picture = document.getElementById('result');
-
-	if (picture) {
-		fetch('/savePicture', {
+	deleteUserPicture(id, image) {
+		fetch('/deleteUserPicture', {
 			method: 'POST',
 			credentials: 'include',
-			body: picture.src
+			body: id
 		})
-		.then(reloadUserPicture);
+		.then(() => {
+			this.userPicturesContainer.removeChild(image);
+		});
 	}
-});
+	
+	appendUserPicture(sources) {
+		if (sources) {
+			const images = sources.map(source => {
+				let image = document.createElement('img');
+				
+				image.src = source['url'];
+				image.classList.add('user-picture');
+				image.addEventListener('click', this.deleteUserPicture.bind(this, source['id'], image));
+				return image;
+			});
+		
+			this.userPicturesContainer.append(...images);
+		}
+	}
+	
+	reloadUserPicture() {
+		this.removeAllChildren(this.userPicturesContainer);
+		fetch('/userPictures', {
+			method: 'POST',
+			credentials: 'include'
+		})
+		.then(response => response.json())
+		.then(this.appendUserPicture)
+		.catch(error => console.log(error.message));
+	}
+
+	renderCamera() {
+		if (navigator.mediaDevices.getUserMedia) {
+			navigator.mediaDevices.getUserMedia({video: true})
+			.then((stream) => {
+				this.video.srcObject = stream;
+			})
+			.catch((err0r) => {
+				console.log('The camera cannot be used');
+			});
+		}
+	}
+
+	takePicture() {
+		this.canvas.width = this.scale;
+		this.canvas.height = (this.video.videoHeight * this.scale) / this.video.videoWidth;
+		this.canvas.getContext('2d').drawImage(this.video, 0, 0, this.canvas.width, this.canvas.height);
+		this.result.src = this.canvas.toDataURL();
+		this.preview.appendChild(this.result);
+	}
+
+	renderStickers() {
+		fetch('/stickers', {
+			method: 'POST',
+			credentials: 'include'
+		})
+		.then(response => response.json())
+		.then(this.appendSticker)
+		.catch(error => console.log(error.message));
+	}
+
+	addSticker(event) {
+		this.removeAllChildren(this.preview);
+		this.canvas.getContext('2d').drawImage(event.target, 0, 0, this.canvas.width, this.canvas.height);
+		this.result.src = this.canvas.toDataURL();
+		this.preview.appendChild(this.result);
+	}
+
+	savePicture() {
+		let picture = document.getElementById('result');
+		
+		if (picture) {
+			fetch('/savePicture', {
+				method: 'POST',
+				credentials: 'include',
+				body: picture.src
+			})
+			.then(this.reloadUserPicture);
+		}
+	}
+
+	render() {
+		this.renderCamera();
+		document.getElementById('account-capture-button').addEventListener('click', this.takePicture);
+		this.renderStickers();
+		this.reloadUserPicture();
+		this.stickersContainer.addEventListener('click', this.addSticker);
+		document.getElementById('account-save-button').addEventListener('click', this.savePicture);
+	}
+}
+
+let account = new Account();
+
+account.render();
