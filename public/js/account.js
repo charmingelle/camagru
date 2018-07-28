adaa49484f6086c002dee068cbacef70da54d553
charmingelle <revenkoaa89@gmail.com>
2018-07-28 11:27:27 +0300

    changed layout

--- public/js/account.js
+++ public/js/account.js
@@ -129,10 +129,12 @@ class Account {
 				image.classList.add('user-photo');
 				image.alt = 'Photo';
 				deleteButton.innerHTML = 'Delete';
+				deleteButton.classList.add('delete-button');
 				deleteButton.addEventListener('click', this.deletePhoto.bind(this, source['id'], imageContainer));
 				twitterDiv.innerHTML = '<blockquote class="twitter-tweet"><p lang="en" dir="ltr">Sunsets don&#39;t get much better than this one over <a href="https://twitter.com/GrandTetonNPS?ref_src=twsrc%5Etfw">@GrandTetonNPS</a>. <a href="https://twitter.com/hashtag/nature?src=hash&amp;ref_src=twsrc%5Etfw">#nature</a> <a href="https://twitter.com/hashtag/sunset?src=hash&amp;ref_src=twsrc%5Etfw">#sunset</a> <a href="http://t.co/YuKy2rcjyU">pic.twitter.com/YuKy2rcjyU</a></p>&mdash; US Department of the Interior (@Interior) <a href="https://twitter.com/Interior/status/463440424141459456?ref_src=twsrc%5Etfw">May 5, 2014</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>';
 				this.renderPublishButton(publishButton, source['id']);
-				imageContainer.append(image, deleteButton, publishButton);
+				publishButton.classList.add('publish-button');
+				imageContainer.append(image, publishButton, deleteButton);
 				return imageContainer;
 			});
 		
