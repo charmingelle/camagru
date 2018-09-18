779ebaecffba57368d3fb875e23c73a26c722da2
charmingelle <revenkoaa89@gmail.com>
2018-09-18 21:36:32 +0300

    deleted unused photos

--- app/core/DBConnect.php
+++ app/core/DBConnect.php
@@ -14,12 +14,8 @@ class DBConnect {
 			}
 		}
 		try {
-			file_put_contents('/Users/grevenko/mamp/apache2/logs/error_log', $query . PHP_EOL, FILE_APPEND);
-			$pdo_statement = self::$_pdo->prepare($query);
-			file_put_contents('/Users/grevenko/mamp/apache2/logs/error_log', implode(' ', $pdo_statement->errorInfo()) . PHP_EOL, FILE_APPEND);			
-			$result = $pdo_statement->execute($params);
-			file_put_contents('/Users/grevenko/mamp/apache2/logs/error_log', implode(' ', $pdo_statement->errorInfo()) . PHP_EOL, FILE_APPEND);
-			file_put_contents('/Users/grevenko/mamp/apache2/logs/error_log', $result . PHP_EOL, FILE_APPEND);
+			$pdo_statement = self::$_pdo->prepare($query);		
+			$pdo_statement->execute($params);
 		} catch (Exception $e) {
 			exit($e->getMessage());
 		}
