e44549bf657efb6fdc8df5c33764cc46c2e5b6fd
charmingelle <revenkoaa89@gmail.com>
2018-09-18 15:51:42 +0300

    changed setup.php

--- app/configs/camagru.sql
+++ app/configs/camagru.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Sep 06, 2018 at 04:33 AM
+-- Generation Time: Sep 18, 2018 at 05:16 AM
 -- Server version: 5.7.21
 -- PHP Version: 7.1.15
 
@@ -30,49 +30,17 @@ USE `camagru`;
 -- Table structure for table `account`
 --
 
-CREATE TABLE `account` (
-  `id` int(11) UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `account` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `email` varchar(128) CHARACTER SET utf8mb4 NOT NULL,
   `login` varchar(128) CHARACTER SET utf8mb4 NOT NULL,
   `password` varchar(128) CHARACTER SET utf8mb4 NOT NULL,
   `hash` varchar(128) CHARACTER SET utf8mb4 DEFAULT NULL,
   `active` tinyint(1) NOT NULL DEFAULT '0',
-  `notification` tinyint(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `account`
---
-
-INSERT INTO `account` (`id`, `email`, `login`, `password`, `hash`, `active`, `notification`) VALUES
-(15, 'vasya123@gmail.com', 'vasya', 'fd9d94340dbd72c11b37ebb0d2a19b4d05e00fd78e4e2ce8923b9ea3a54e900df181cfb112a8a73228d1f3551680e2ad9701a4fcfb248fa7fa77b95180628bb2', NULL, 1, 1),
-(16, 'kolya@gmail.com', 'kolya', 'fd9d94340dbd72c11b37ebb0d2a19b4d05e00fd78e4e2ce8923b9ea3a54e900df181cfb112a8a73228d1f3551680e2ad9701a4fcfb248fa7fa77b95180628bb2', NULL, 1, 1),
-(17, 'sasha@gmail.com', 'sasha', 'fd9d94340dbd72c11b37ebb0d2a19b4d05e00fd78e4e2ce8923b9ea3a54e900df181cfb112a8a73228d1f3551680e2ad9701a4fcfb248fa7fa77b95180628bb2', NULL, 1, 1),
-(19, 'liza@gmail.com', 'liza', 'fd9d94340dbd72c11b37ebb0d2a19b4d05e00fd78e4e2ce8923b9ea3a54e900df181cfb112a8a73228d1f3551680e2ad9701a4fcfb248fa7fa77b95180628bb2', NULL, 1, 1),
-(37, 'olya@gmail.com', 'olya', 'fd9d94340dbd72c11b37ebb0d2a19b4d05e00fd78e4e2ce8923b9ea3a54e900df181cfb112a8a73228d1f3551680e2ad9701a4fcfb248fa7fa77b95180628bb2', NULL, 1, 1),
-(41, 'annar703unit@gmail.com', 'anna', 'fd9d94340dbd72c11b37ebb0d2a19b4d05e00fd78e4e2ce8923b9ea3a54e900df181cfb112a8a73228d1f3551680e2ad9701a4fcfb248fa7fa77b95180628bb2', NULL, 1, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `comment`
---
-
-CREATE TABLE `comment` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `comment` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
-  `photo_id` int(11) UNSIGNED NOT NULL,
-  `login` varchar(128) CHARACTER SET utf8mb4 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `comment`
---
-
-INSERT INTO `comment` (`id`, `comment`, `photo_id`, `login`) VALUES
-(1, 'comment', 2, 'anna'),
-(2, 'anya\'s comment', 2, 'anna'),
-(3, 'olya\'s comment', 2, 'olya');
+  `notification` tinyint(1) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login` (`login`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -80,28 +48,12 @@ INSERT INTO `comment` (`id`, `comment`, `photo_id`, `login`) VALUES
 -- Table structure for table `likes`
 --
 
-CREATE TABLE `likes` (
-  `id` int(11) UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `likes` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `photo_id` int(11) UNSIGNED NOT NULL,
-  `login` varchar(128) CHARACTER SET utf8mb4 NOT NULL
+  `login` varchar(128) CHARACTER SET utf8mb4 NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `likes`
---
-
-INSERT INTO `likes` (`id`, `photo_id`, `login`) VALUES
-(6, 11, 'vasya'),
-(12, 11, 'kolya'),
-(17, 11, 'anna'),
-(22, 12, 'anna'),
-(24, 18, 'anna'),
-(26, 17, 'anna'),
-(51, 28, 'anna'),
-(52, 28, 'maksim'),
-(55, 28, 'vasya'),
-(60, 2, 'anna'),
-(61, 1, 'olya');
 
 -- --------------------------------------------------------
 
@@ -109,23 +61,30 @@ INSERT INTO `likes` (`id`, `photo_id`, `login`) VALUES
 -- Table structure for table `photo`
 --
 
-CREATE TABLE `photo` (
-  `id` int(11) UNSIGNED NOT NULL,
+CREATE TABLE IF NOT EXISTS `photo` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `url` varchar(2083) CHARACTER SET utf8mb4 NOT NULL,
   `likes` int(11) NOT NULL DEFAULT '0',
   `comments` int(11) NOT NULL DEFAULT '0',
   `login` varchar(128) CHARACTER SET utf8mb4 NOT NULL,
-  `private` tinyint(4) NOT NULL DEFAULT '1'
+  `private` tinyint(4) NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `photo`
+-- Table structure for table `comment`
 --
 
-INSERT INTO `photo` (`id`, `url`, `likes`, `comments`, `login`, `private`) VALUES
-(2, 'photo/da8e917bc85d4a29.png', 0, 3, 'anna', 0),
-(3, 'photo/ff0995fc208df4ad.png', 0, 0, 'anna', 1),
-(4, 'photo/b496695fdade6d73.png', 0, 0, 'anna', 1);
+CREATE TABLE IF NOT EXISTS `comment` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `comment` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+  `photo_id` int(11) UNSIGNED NOT NULL,
+  `login` varchar(128) CHARACTER SET utf8mb4 NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `photoID_commentID` FOREIGN KEY (`photo_id`) REFERENCES `photo` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -133,14 +92,17 @@ INSERT INTO `photo` (`id`, `url`, `likes`, `comments`, `login`, `private`) VALUE
 -- Table structure for table `sticker`
 --
 
-CREATE TABLE `sticker` (
-  `id` int(11) UNSIGNED NOT NULL,
-  `url` varchar(2083) CHARACTER SET utf8mb4 NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE IF NOT EXISTS `sticker` (
+  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `url` varchar(2083) CHARACTER SET utf8mb4 NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `sticker`
 --
+
+DELETE FROM `sticker`;
 
 INSERT INTO `sticker` (`id`, `url`) VALUES
 (1, 'sticker/1.png'),
@@ -156,88 +118,3 @@ INSERT INTO `sticker` (`id`, `url`) VALUES
 (11, 'sticker/11.svg'),
 (12, 'sticker/12.png'),
 (13, 'sticker/13.png');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `account`
---
-ALTER TABLE `account`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `login` (`login`);
-
---
--- Indexes for table `comment`
---
-ALTER TABLE `comment`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `photoID_commentID` (`photo_id`);
-
---
--- Indexes for table `likes`
---
-ALTER TABLE `likes`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `photo`
---
-ALTER TABLE `photo`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `sticker`
---
-ALTER TABLE `sticker`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `account`
---
-ALTER TABLE `account`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
-
---
--- AUTO_INCREMENT for table `comment`
---
-ALTER TABLE `comment`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `likes`
---
-ALTER TABLE `likes`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
-
---
--- AUTO_INCREMENT for table `photo`
---
-ALTER TABLE `photo`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `sticker`
---
-ALTER TABLE `sticker`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `comment`
---
-ALTER TABLE `comment`
-  ADD CONSTRAINT `photoID_commentID` FOREIGN KEY (`photo_id`) REFERENCES `photo` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- app/configs/setup.php
+++ app/configs/setup.php
@@ -1,4 +1,72 @@
 <?php
-	require_once('./database.php');
+	require_once('database.php');
+	require_once('./../core/DBConnect.php');
 
-	exec(MYSQL_PATH . " -u " . DB_USER . " -p < ./camagru.sql");
+	DBConnect::sendQuery('SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";');
+	DBConnect::sendQuery('SET AUTOCOMMIT = 0;');
+	DBConnect::sendQuery('START TRANSACTION;');
+	DBConnect::sendQuery('SET time_zone = "+00:00";');
+	
+	DBConnect::sendQuery('CREATE DATABASE IF NOT EXISTS `camagru` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+	USE `camagru`;');
+	
+	DBConnect::sendQuery('CREATE TABLE IF NOT EXISTS `account` (
+	  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	  `email` varchar(128) CHARACTER SET utf8mb4 NOT NULL,
+	  `login` varchar(128) CHARACTER SET utf8mb4 NOT NULL,
+	  `password` varchar(128) CHARACTER SET utf8mb4 NOT NULL,
+	  `hash` varchar(128) CHARACTER SET utf8mb4 DEFAULT NULL,
+	  `active` tinyint(1) NOT NULL DEFAULT "0",
+	  `notification` tinyint(1) NOT NULL DEFAULT "1",
+	  PRIMARY KEY (`id`),
+	  UNIQUE KEY `login` (`login`)
+	) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;');
+	
+	DBConnect::sendQuery('CREATE TABLE IF NOT EXISTS `likes` (
+	  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	  `photo_id` int(11) UNSIGNED NOT NULL,
+	  `login` varchar(128) CHARACTER SET utf8mb4 NOT NULL,
+	  PRIMARY KEY (`id`)
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
+	
+	DBConnect::sendQuery('CREATE TABLE IF NOT EXISTS `photo` (
+	  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	  `url` varchar(2083) CHARACTER SET utf8mb4 NOT NULL,
+	  `likes` int(11) NOT NULL DEFAULT "0",
+	  `comments` int(11) NOT NULL DEFAULT "0",
+	  `login` varchar(128) CHARACTER SET utf8mb4 NOT NULL,
+	  `private` tinyint(4) NOT NULL DEFAULT "1",
+	  PRIMARY KEY (`id`)
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
+	
+	DBConnect::sendQuery('CREATE TABLE IF NOT EXISTS `comment` (
+	  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	  `comment` varchar(255) CHARACTER SET utf8mb4 NOT NULL,
+	  `photo_id` int(11) UNSIGNED NOT NULL,
+	  `login` varchar(128) CHARACTER SET utf8mb4 NOT NULL,
+	  PRIMARY KEY (`id`),
+	  CONSTRAINT `photoID_commentID` FOREIGN KEY (`photo_id`) REFERENCES `photo` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+	) ENGINE=InnoDB DEFAULT CHARSET=utf8;');
+	
+	DBConnect::sendQuery('CREATE TABLE IF NOT EXISTS `sticker` (
+	  `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+	  `url` varchar(2083) CHARACTER SET utf8mb4 NOT NULL,
+	  PRIMARY KEY (`id`)
+	) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;');
+	
+	DBConnect::sendQuery('DELETE FROM `sticker`;');
+	
+	DBConnect::sendQuery('INSERT INTO `sticker` (`id`, `url`) VALUES
+	(1, "sticker/1.png"),
+	(2, "sticker/2.png"),
+	(3, "sticker/3.png"),
+	(4, "sticker/4.png"),
+	(5, "sticker/5.png"),
+	(6, "sticker/6.png"),
+	(7, "sticker/7.png"),
+	(8, "sticker/8.jpg"),
+	(9, "sticker/9.png"),
+	(10, "sticker/10.png"),
+	(11, "sticker/11.svg"),
+	(12, "sticker/12.png"),
+	(13, "sticker/13.png");');
